0148564cfee651bdb74e612031c61b1bec502789
s2quake <han0210@netsgo.com>
2024-01-10 16:07:10 +0900

    feat: Added algorithm 5-4, failed

--- DistributedLedgers.ConsoleHost/Commands/Chapter5/Algorithm_5_4Command.cs
+++ DistributedLedgers.ConsoleHost/Commands/Chapter5/Algorithm_5_4Command.cs
@@ -1,11 +1,13 @@
 
 using System.ComponentModel.Composition;
+using System.Xml.Serialization;
+using DistributedLedgers.ConsoleHost.Common;
 using JSSoft.Commands;
 
 namespace DistributedLedgers.ConsoleHost.Commands.Chapter5;
 
 [Export(typeof(ICommand))]
-sealed class Algorithm_5_4Command : CommandBase
+sealed class Algorithm_5_4Command : CommandAsyncBase
 {
     public Algorithm_5_4Command()
         : base("alg-5-4")
@@ -13,41 +15,71 @@ sealed class Algorithm_5_4Command : CommandBase
     }
 
     [CommandPropertyRequired(DefaultValue = ulong.MaxValue)]
-    public ulong PrimeNumber
-    {
-        get; set;
-    }
+    public ulong PrimeNumber { get; set; }
 
-    protected override void OnExecute()
+    protected override async Task OnExecuteAsync(CancellationToken cancellationToken, IProgress<ProgressInfo> progress)
     {
         var p = PrimeNumber == ulong.MaxValue ? GetOddNumber() : PrimeNumber;
         if (p % 2 == 0)
             throw new InvalidOperationException($"'{p}' is not odd number.");
 
-        var j = GetOddNumber();
+        var z = new ulong[]
+        {
+            11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+        };
+        p = z[Random.Shared.Next(z.Length)];
 
-        p = 13;
-        j = 9;
-        var r = Math.Log((p - 1) / j) / Math.Log(2);
-        var x = GetX(p);
+        // p = 17;
+        // p = 31;
 
+        var js = GetValues(p).ToArray();
+        if (js.Length == 0)
+            throw new InvalidOperationException($"{p} is not prime number -- ");
+        var j = js[Random.Shared.Next(js.Length)];
+        var r = (ulong)Math.Log((p - 1) / 2, 2);
+        var x = RandomUtility.NextULong(1, p);
+        var x0 = Math.Pow(x, j) % p;
+        if (x0 == 1 || x0 == (p - 1))
+        {
+            Out.WriteLine($"{p} is prime number: step 1");
+            return;
+        }
 
-        Out.WriteLine($"{p}, {x}");
+        for (var i = 1ul; i <= r - 1; i++)
+        {
+            var xi = Math.Pow(x0, 2) % p;
+            if (xi == p - 1)
+            {
+                Out.WriteLine($"{p} is prime number: step 2");
+                return;
+            }
+            x0 = xi;
+        }
+
+        throw new InvalidOperationException($"{p} is not prime number");
+    }
+
+    private static IEnumerable<ulong> GetValues(ulong primeNumber)
+    {
+        var p = primeNumber;
+        for (ulong i = 1; i < p - 1; i += 2)
+        {
+            var j = i;
+            var r = Math.Log((p - 1) / j, 2);
+            if ((r % 1 == 0) && (p - 1) == Math.Pow(2, r) * j)
+            {
+                yield return j;
+            }
+        }
     }
 
     private static ulong GetOddNumber()
     {
         var i = ulong.MaxValue;
-        while ((i = (ulong)Random.Shared.NextInt64()) % 2 == 0)
+        do
         {
-        }
+            i = RandomUtility.NextULong();
+        } while (i % 2 == 0);
         return i;
-    }
-
-    private static ulong GetX(ulong p)
-    {
-        var min = 1L - long.MaxValue;
-        var max = (long)(p - long.MaxValue);
-        return (uint)(Random.Shared.NextInt64(min, max) + long.MaxValue);
     }
 }
--- DistributedLedgers.ConsoleHost/Common/RandomUtility.cs
+++ DistributedLedgers.ConsoleHost/Common/RandomUtility.cs
@@ -2,15 +2,39 @@ namespace DistributedLedgers.ConsoleHost.Common;
 
 static class RandomUtility
 {
+    public static ulong NextULong() => NextULong(ulong.MinValue, ulong.MaxValue);
+
+    public static ulong NextULong(ulong max) => NextULong(ulong.MinValue, max);
+
     public static ulong NextULong(ulong min, ulong max)
     {
-        var hight = Random.Shared.Next((int)(min >> 32), (int)(max >> 32));
-        var minLow = Math.Min((int)min, (int)max);
-        var maxLow = Math.Max((int)min, (int)max);
-        var low = (uint)Random.Shared.Next(minLow, maxLow);
-        ulong result = (ulong)hight;
-        result <<= 32;
-        result |= (ulong)low;
-        return result;
+        var m1 = (long)(min - long.MaxValue - 1);
+        var m2 = (long)(max - long.MaxValue - 1);
+        var m = Random.Shared.NextInt64(m1, m2);
+        var v = (ulong)(m + long.MaxValue + 1);
+        return v;
+    }
+
+    public static byte NextByte(byte min, byte max)
+    {
+        var m1 = (sbyte)(min - sbyte.MaxValue - 1);
+        var m2 = (sbyte)(max - sbyte.MaxValue - 1);
+        var m = (sbyte)Random.Shared.Next(m1, m2);
+        var v = (byte)(m + sbyte.MaxValue + 1);
+        return v;
+    }
+
+    public static ulong NextULong(ulong min, ulong max, Predicate<ulong> predicate)
+    {
+        var tryCount = 10;
+        for (var i = 0; i < tryCount; i++)
+        {
+            var v = NextULong(min, max);
+            if (predicate(v) == true)
+            {
+                return v;
+            }
+        }
+        throw new NotImplementedException("Cannot found value that matches condition.");
     }
 }
